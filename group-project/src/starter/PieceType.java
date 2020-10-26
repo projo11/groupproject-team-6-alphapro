7ad9f3763e86f62d1b0590d0abcf9cf10c822fb9
Jordan <jorda@DESKTOP-1ML1212.lan>
2020-10-26 11:22:20 -0700

    Imported files from old repository.

---
+++ group-project/src/starter/PieceType.java
@@ -0,0 +1,5 @@
+package starter;
+
+public enum PieceType {
+	PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING;
+}
---
+++ group-project/src/starter/Space.java
@@ -0,0 +1,24 @@
+package starter;
+
+public class Space {
+	private int row;
+	private int col;
+
+	public Space(int r, int c) { row = r; col = c; }
+	
+	
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getCol() {
+		return col;
+	}
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+}
+

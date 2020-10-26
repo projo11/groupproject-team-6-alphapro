4a39b721841f905db388e8150d4635fb2d62f9c6
Weiming <w_kong@u.pacific.edu>
2020-10-27 04:03:18 +0900

    Start graphics games

--- group-project/src/starter/GraphicsGame.java
+++ group-project/src/starter/GraphicsGame.java
@@ -1,5 +1,43 @@
 package starter;
 
-public class GraphicsGame {
+import java.awt.Color;
+import java.awt.event.MouseEvent;
 
+import acm.graphics.GLabel;
+import acm.graphics.GPoint;
+
+public class GraphicsGame {
+	private board Board;
+	private int clickX, clickY, releaseX, releaseY, lastX, lastY;
+	public void printTitleScreen() {
+		
+	}
+	
+	public void printRules1() {
+		
+	}
+	public void printRules2() {
+		
+	}
+	public void printBoard() {
+	
+	}
+	public void printPieceShop() {
+		
+	}
+    public void printWiunScreen() {
+		
+	}
+    @Override
+    public void mousePressed(MouseEvent e) {
+    	
+	}
+    @Override
+    public void mouseDragged(MouseEvent e) {
+    
+    }
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+	}
 }

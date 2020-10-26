40505880993e38b2f112c1b080db1a08d442f8b9
Jordan <jorda@DESKTOP-1ML1212.lan>
2020-10-26 11:57:12 -0700

    Added the function declarations for the Board class.

--- group-project/src/starter/Board.java
+++ group-project/src/starter/Board.java
@@ -11,5 +11,34 @@ public boolean isOutOfBounds(Space s) {
 	return false;
 }
 
+public boolean moveNumSpaces(Space start, int r, int c) {
+	//Moves the piece at the given space down by r and to the right by c.Returns false if the space given is null.
+	return true;
+}
+
+public void addPiece(PieceType type, int r, int c, boolean isWhite) {
+	//Creates a Piece object using the arguments and adds it to the board at the coordinates given.
+	//Be careful calling this function, because it will override any piece that is currently at that location.
+	//If desired, could be designed to return false if the location given is not null.
+}
+
+public boolean canMoveNumSpaces(Space start, int r, int c) {
+	//Returns true if the location at start, translated down by r and to the right by c, is null.
+	return true;
+}
+
+public Piece getPiece(Space s) {
+	//Returns the piece at the space on the board given.
+	return new Piece();
+}
+
+public void swapPieces(Space s1, Space s2) {
+	//Switches the location of the two pieces given. Should only be called when setting up the board pre-game, 
+	// and only if both spaces contain pieces of the same color.
+}
+
+public void flipBoard() {
+	//Reverses the 2-d array board. Used to simulate looking at the board from the opposite perspective.
+}
 
 }

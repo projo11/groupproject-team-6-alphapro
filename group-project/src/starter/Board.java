73f0f2b5c178d749145a7f4c3e556e9ef1911a6a
jorda <jorda@DESKTOP-1ML1212.lan>
2020-10-26 11:33:58 -0700

    Implemented the isOutOfBounds board function.

--- group-project/src/starter/Board.java
+++ group-project/src/starter/Board.java
@@ -1,5 +1,15 @@
 package starter;
 
 public class Board {
+private Space board[][];
+
+//Returns true if the Space given has coordinates that are not on the board.
+public boolean isOutOfBounds(Space s) {
+	if (s.getRow() < 0 || s.getRow() > 7 || s.getCol() < 0 || s.getCol() > 7) {
+		return true;
+	}
+	return false;
+}
+
 
 }

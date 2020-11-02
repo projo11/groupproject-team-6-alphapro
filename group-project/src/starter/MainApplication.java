6f91e097662a9b661359bdea9f69623a82d28b50
Dwight <d_soriacallupe@u.pacific.edu>
2020-11-01 16:04:58 -0800

    Wrote comments on MainApplication.java to understand and break down the file. Also wrote suggestions to what we can do in our own project.

--- group-project/src/starter/MainApplication.java
+++ group-project/src/starter/MainApplication.java
@@ -1,12 +1,12 @@
 package starter;
 public class MainApplication extends GraphicsApplication {
-	public static final int WINDOW_WIDTH = 800;
+	public static final int WINDOW_WIDTH = 800; //This is where you define the window with and height for the application. For our app we'll probably make it a bit bigger.
 	public static final int WINDOW_HEIGHT = 600;
-	public static final String MUSIC_FOLDER = "sounds";
-	private static final String[] SOUND_FILES = { "r2d2.mp3", "somethinlikethis.mp3" };
+	public static final String MUSIC_FOLDER = "sounds"; //Basically "sounds" is the name of the folder that you put the .mp3 files into. We can name our folder something else but sounds makes the most sense
+	private static final String[] SOUND_FILES = { "r2d2.mp3", "somethinlikethis.mp3" }; //These are just the sound files in the sounds folder, in our case we would have a sound for the menu, game screen, and end screen.
 
-	private SomePane somePane;
-	private MenuPane menu;
+	private SomePane somePane; //Basically a some window; For our project we could put something like private InstrPane instructions;
+	private MenuPane menu; //Menu window ; We can use the same format for our menu page
 	private int count;
 
 	public void init() {
@@ -15,18 +15,18 @@ public class MainApplication extends GraphicsApplication {
 
 	public void run() {
 		System.out.println("Hello, world!");
-		somePane = new SomePane(this);
+		somePane = new SomePane(this); //initialzing
 		menu = new MenuPane(this);
 		switchToMenu();
 	}
 
-	public void switchToMenu() {
-		playRandomSound();
+	public void switchToMenu() { // function to switch screen to menu
+		playRandomSound(); // plays a sound when switched
 		count++;
 		switchToScreen(menu);
 	}
 
-	public void switchToSome() {
+	public void switchToSome() { //function to switch the screen to some screen we can use this to switch to each window
 		playRandomSound();
 		switchToScreen(somePane);
 	}

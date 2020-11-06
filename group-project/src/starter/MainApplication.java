57fb6dcc4dee794c37e9305c788a988ba54911f7
Dwight <d_soriacallupe@u.pacific.edu>
2020-11-06 11:43:51 -0800

    Added code to MainApplication.java

--- group-project/src/starter/MainApplication.java
+++ group-project/src/starter/MainApplication.java
@@ -36,3 +36,42 @@ public class MainApplication extends GraphicsApplication {
 		audio.playSound(MUSIC_FOLDER, SOUND_FILES[count % SOUND_FILES.length]);
 	}
 }
+
+/*
+public class MainApplication extends GraphicsApplication{
+public static final int WINDOW_WIDTH = 900;
+public static final int WINDOW_HEIGHT = 900;
+public static final String MUSIC_FOLDER = "sounds";
+private static final String [] SOUND_FILES = { "menumusic.mp3", "chessgamemusic.mp3"};
+
+private SomePane somePane;
+private MenuPane menu;
+private int count;
+
+public void init(){
+	setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	}
+	
+public void run() {
+	System.out.println("Hello world!");
+	somePane = new SomePane(this);
+	menu = new MenuPane(this);
+	switchToMenu();
+	}
+
+public void switchToMenu() {
+playSound();
+count++;
+switchToScreen(menu);
+}
+
+}
+
+
+
+
+
+
+
+
+*/

488044d6fd590daa1387bebab10e45c5d1bc509f
Alexandra <a_hum@u.pacific.edu>
2020-11-15 15:44:45 -0800

    Trying to implement and fix code from GraphicsGame.java to ChessBoardPane.java

--- group-project/src/starter/ChessboardPane.java
+++ group-project/src/starter/ChessboardPane.java
@@ -7,14 +7,14 @@ import acm.util.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import acm.graphics.GImage;
-import acm.graphics.GRect;
+import java.awt.event.MouseEvent;
 
 public class ChessboardPane extends GraphicsPane implements ActionListener {
 private MainApplication program;
 public static final int SPACE_WIDTH = 40;
 public static final int SPACE_HEIGHT = 40;
+private int clickX, clickY, lastX, lastY;
+private GObject toDrag;
 	
 public ChessboardPane(MainApplication app) {
 		program = app;
@@ -152,6 +152,114 @@ for (Piece temp : program.getBoard().getPieces()) {
 		program.removeAll();
 		
 	}
+	
+	 @Override
+	    public void mousePressed(MouseEvent e) {
+	    	toDrag = getElementAt(e.getX(), e.getY());
+	    	lastX = e.getX();
+	    	lastY = e.getY();
+	    	clickX = e.getX();
+	    	clickY = e.getY();
+		}
+	    @Override
+	    public void mouseDragged(MouseEvent e) {
+	    	if(toDrag != null)
+	    	{
+	    		toDrag.move(e.getX()-lastX, e.getY()-lastY);
+	    	}
+	    	lastX = e.getX();
+	    	lastY = e.getY();
+	    }
+	    @Override
+		public void mouseReleased(MouseEvent e) {
+	    	Space space = convertXYToSpace(clickX, clickY);
+	    	Piece piece = getPieceFromXY(clickX, clickY);
+	    	//TODO: Figure out where to make it so that isPlayingMatch becomes true
+	    		if(piece != null)
+	        	{
+	        		program.getBoard().moveNumSpaces(space, calculateRowsMoved(), calculateColsMoved());
+	        		removeAll();
+	        		printBoard(); 
+	        	}
+	        	if(piece.getColor() == true) //true = white -> piece is white
+	        	{
+	        		if(board.checkmate(piece.getColor()) == true) //if a white piece puts the opponent in checkmate
+	        		{
+	        			removeAll();
+	        			printWinScreen();
+	        		}
+	        	}
+	        	else //false = black -> piece is black
+	        	{
+	        		if(board.checkmate(piece.getColor()) == true) //if a black piece puts the opponent in checkmate
+	        		{
+	        			removeAll();
+	        			printWinScreen();
+	        		}
+	        	}
+	    }
+	    
+	    //Below are functions used to help with the mouse listener functions
+	    //Code that converts XY coordinates into a Space
+	    private Space convertXYToSpace(double x, double y)
+	    {
+	    	Space space = new Space((int)(y/SPACE_HEIGHT), (int)(x/SPACE_WIDTH)); 
+	    	return space;
+	    }
+	    
+	    //Code that returns the Piece in a given space with the provided XY coordinates
+	    private Piece getPieceFromXY(double x, double y)
+	    {
+	    	Space space = convertXYToSpace(x, y);
+	    	return board.getPiece(space);
+	    }
+	    
+	    //TODO: When calculating calculating rows moved and cols moved, must change it to account
+	    //		for the BOARD_SHIFT of 100 pixels; ANY pieces that are moved outside of the board
+	    //		is not allowed
+	    //Code that returns how many rows a piece has moved
+	    private int calculateRowsMoved()
+	    {
+	    	int rowsMoved = 0;
+	    	Space start = convertXYToSpace(clickX, clickY); 
+	    	Space end = convertXYToSpace(lastX, lastY); 
+	    	Piece piece = getPieceFromXY(clickX, clickY);
+	    	if(piece != null)
+	    	{
+	    		rowsMoved = end.getRow() - start.getRow();
+	    	}
+	    	else if(lastX <= BOARD_SHIFT) //If the place the player moved the chess piece is out of bounds, then the move is invalid the the rows moved is 0
+	    	{
+	    		rowsMoved = 0;
+	    	}
+	    	else //If no piece was selected, the amount of rows moved is 0
+	    	{
+	    		rowsMoved = 0;
+	    	}
+	    	return rowsMoved;
+	    }
+	    
+	  //Code that returns how many cols a piece has moved
+	    private int calculateColsMoved()
+	    {
+	    	int colsMoved = 0;
+	    	Space start = convertXYToSpace(clickX, clickY);
+	    	Space end = convertXYToSpace(lastX, lastY);
+	    	Piece piece = getPieceFromXY(clickX, clickY);
+	    	if(piece != null)
+	    	{
+	    		colsMoved = end.getCol() - start.getCol();
+	    	}
+	    	else if(lastY <= BOARD_SHIFT) //If the place the player moved the chess piece is out of bounds, then the move is invalid the the cols moved is 0
+	    	{
+	    		colsMoved = 0;
+	    	}
+	    	else //If no piece was selected, the amount of cols moved is 0
+	    	{
+	    		colsMoved = 0;
+	    	}
+	    	return colsMoved;
+	    }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {

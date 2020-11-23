b96f82ad0a3736ed0e48e1c4222babbfaa4ffbfa
Weiming <w_kong@u.pacific.edu>
2020-11-23 09:01:49 +0900

    Put pieces player bought on the board

--- group-project/src/starter/PieceShopPane.java
+++ group-project/src/starter/PieceShopPane.java
@@ -2,9 +2,11 @@ package starter;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,6 +16,7 @@ import javax.swing.JPanel;
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
+import acm.graphics.GPoint;
 import acm.graphics.GRect;
 
 public class PieceShopPane extends GraphicsPane implements ActionListener {
@@ -23,11 +26,11 @@ private MainApplication program;
 private GButton P1 = new GButton("P1 Done", 700, 700, 150 , 80);
 private GButton P2 = new GButton("P2 Done", 700, 700, 150 , 80);
 //buy pieces button
-private GButton Bp = new GButton("Buy", 820, 80, 70, 30);
-private GButton BK = new GButton("Buy", 820, 120, 70, 30);
-private GButton Br = new GButton("Buy", 820, 160, 70, 30);
-private GButton Bb = new GButton("Buy", 820, 200, 70, 30);
-private GButton Bq = new GButton("Buy", 820, 240, 70, 30);
+private final GButton Bp = new GButton("Buy", 820, 80, 70, 30);
+private final GButton BK = new GButton("Buy", 820, 120, 70, 30);
+private final GButton Br = new GButton("Buy", 820, 160, 70, 30);
+private final GButton Bb = new GButton("Buy", 820, 200, 70, 30);
+private final GButton Bq = new GButton("Buy", 820, 240, 70, 30);
 //detect player 1 or 2
 private int player = 1;
 public static final int SPACE_SIZE = 77;
@@ -37,8 +40,15 @@ public static final double PAWN_SIZE_MOD = 0.55;
 public static final String LABEL_FONT = "Arial-Bold-22";
 public static final Color LABEL_COLOR = Color.red;
 
+private Board p = new Board();
+
 private int clickX, clickY, lastX, lastY;
+private int startX;
+private int startY;
 private GObject toDrag;
+private int pieceT;
+GImage toAdd;
+String filePath;
 
 public PieceShopPane(MainApplication app) {
 		program = app;
@@ -95,7 +105,7 @@ public PieceShopPane(MainApplication app) {
 			}
 			else {
 				toAdd = new GImage(filePath, temp.getCol()*SPACE_SIZE + BOARD_SHIFT + (((1-SIZE_MOD)*SPACE_SIZE)/2), temp.getRow()*SPACE_SIZE + BOARD_SHIFT + (((1-SIZE_MOD)*SPACE_SIZE)/2));
-				toAdd.setSize(SPACE_SIZE*SIZE_MOD, SPACE_SIZE*SIZE_MOD);
+				toAdd.setSize(SPACE_SIZE * PAWN_SIZE_MOD, SPACE_SIZE * PAWN_SIZE_MOD);
 			}
 			program.add(toAdd);
 			}
@@ -231,42 +241,236 @@ public PieceShopPane(MainApplication app) {
 			program.switchToGame();
 		}
 		if(obj == Bp) {//add pawn
-			GImage toAdd;
-			String filePath;
-			System.out.print("Enter the location of the Piece():");
-			toDrag = program.getElementAt(e.getX(), e.getY());
-			lastX = e.getX();
-			lastY = e.getY();
-			//System.out.println("X:" + lastX + " Y:"+lastY);
+			pieceT = 0;
 			if(player == 2) {
-				Piece p = new Piece(lastX,lastX,PieceType.PAWN, false);
 				filePath = new String("Black_P.png");
 				toAdd = new GImage(filePath);
-				toAdd.setBounds(lastX, lastY, SPACE_SIZE*SIZE_MOD, SPACE_SIZE*SIZE_MOD);
+				toAdd.setBounds(lastX, lastY, 42,42);
 			}
 			else {
-				Piece p = new Piece(lastX,lastX,PieceType.PAWN, true);
 				filePath = new String("White_P.png");
 				toAdd = new GImage(filePath);
-				toAdd.setBounds(lastX, lastY, SPACE_SIZE*SIZE_MOD, SPACE_SIZE*SIZE_MOD);
+				toAdd.setBounds(lastX, lastY, 42,42);
 			}
 			program.add(toAdd);
 				
 		}
 		if(obj == BK) {//add knight
-			
+			pieceT = 1;
+			if(player == 2) {
+				filePath = new String("Black_Knight.png");
+				toAdd = new GImage(filePath);
+				toAdd.setBounds(lastX, lastY, 42,42);
+			}
+			else {
+				filePath = new String("White_Knight.png");
+				toAdd = new GImage(filePath);
+				toAdd.setBounds(lastX, lastY, 42,42);
+			}
+			program.add(toAdd);
 		}
 		if(obj == Br) {//add rook
-			
+			pieceT = 2;
+			if(player == 2) {
+				filePath = new String("Black_Rook.png");
+				toAdd = new GImage(filePath);
+				toAdd.setBounds(lastX, lastY, 42,42);
+			}
+			else {
+				filePath = new String("White_Rook.png");
+				toAdd = new GImage(filePath);
+				toAdd.setBounds(lastX, lastY, 42,42);
+			}
+			program.add(toAdd);
 		}
 		if(obj == Bb) {//add bishop
-			
+			pieceT = 3;
+			if(player == 2) {
+				filePath = new String("Black_Bishop.png");
+				toAdd = new GImage(filePath);
+				toAdd.setBounds(lastX, lastY, 42,42);
+			}
+			else {
+				filePath = new String("White_Bishop.png");
+				toAdd = new GImage(filePath);
+				toAdd.setBounds(lastX, lastY, 42,42);
+			}
+			program.add(toAdd);
 		}
 		if(obj == Bq) {//add queen
-			
+			pieceT = 4;
+			if(player == 2) {
+				filePath = new String("Black_Queen.png");
+				toAdd = new GImage(filePath);
+				toAdd.setBounds(lastX, lastY, 42,42);
+			}
+			else {
+				filePath = new String("White_Queen.png");
+				toAdd = new GImage(filePath);
+				toAdd.setBounds(lastX, lastY, 42,42);
+			}
+			program.add(toAdd);
+		}
+		if(obj == toAdd) {
+				toDrag = program.getElementAt(e.getX(), e.getY());
+			    //lastX = e.getX();
+			    //lastY = e.getY();
+			    //add pawn
+			    /*if(pieceT == 0 && player == 2){
+			    	piece.addPiece(lastX,lastY,PieceType.PAWN, false);
+			    }
+			    if(pieceT == 0 && player == 1){
+			    	piece.addPiece(lastX,lastY,PieceType.PAWN, true);
+			    }
+			    //add knight
+			    if(pieceT == 1 && player == 2){
+			    	piece.addPiece(lastX,lastY,PieceType.KNIGHT, false);
+			    }
+			    if(pieceT == 1 && player == 1){
+			    	piece.addPiece(lastX,lastY,PieceType.KNIGHT, true);
+			    }
+			    //add rook
+			    if(pieceT == 2 && player == 2){
+			    	piece.addPiece(lastX,lastY,PieceType.ROOK, false);
+			    }
+			    if(pieceT == 2 && player == 1){
+			    	piece.addPiece(lastX,lastY,PieceType.ROOK, true);
+			    }
+			    //add bishop
+			    if(pieceT == 3 && player == 2){
+			    	piece.addPiece(lastX,lastY,PieceType.BISHOP, false);
+			    }
+			    if(pieceT == 3 && player == 1){
+			    	piece.addPiece(lastX,lastY,PieceType.PAWN, true);
+			    }
+			    //add queen
+			    if(pieceT == 4 && player == 2){
+			    	piece.addPiece(lastX,lastY,PieceType.QUEEN, false);
+			    }
+			    if(pieceT == 4 && player == 1){
+			    	piece.addPiece(lastX,lastY,PieceType.QUEEN, true);
+			    }*/
 		}
 	}
 	
+
+	@Override
+    public void mouseDragged(MouseEvent e) {
+		if(toDrag != null)
+	    {
+	    	toDrag.move(e.getX()-lastX, e.getY()-lastY);
+	    }
+	    lastX = e.getX();
+	    lastY = e.getY();
+    }
+	
+	//TODO: Fix up the code so that when it reprint the board it doesn't go back to the original;
+	//		try using the addPiece() function in Board.java for this.
+	//		Fix up the check to see if the piece moves out of bounds; the movement check is also
+	//		not working properly and needs to be fixed.
+	//		Fix the the board so you can't drag around the square tiles
+
+    @Override
+  	public void mouseReleased(MouseEvent e) {
+      	Space space = convertXYToSpace(clickX, clickY);
+      	Piece piece;
+      	
+      	if(space.getRow() != -1 && space.getCol() != -1)
+      	{
+      		piece = getPieceFromXY(clickX, clickY);
+      	}
+      	lastX = space.getRow();
+      	lastY = space.getCol();
+      //add pawn
+	    /*if(pieceT == 0 && player == 2){
+	    	piece.addPiece(lastX,lastY,PieceType.PAWN, false);
+	    }
+	    if(pieceT == 0 && player == 1){
+	    	piece.addPiece(lastX,lastY,PieceType.PAWN, true);
+	    }
+	    //add knight
+	    if(pieceT == 1 && player == 2){
+	    	piece.addPiece(lastX,lastY,PieceType.KNIGHT, false);
+	    }
+	    if(pieceT == 1 && player == 1){
+	    	piece.addPiece(lastX,lastY,PieceType.KNIGHT, true);
+	    }
+	    //add rook
+	    if(pieceT == 2 && player == 2){
+	    	piece.addPiece(lastX,lastY,PieceType.ROOK, false);
+	    }
+	    if(pieceT == 2 && player == 1){
+	    	piece.addPiece(lastX,lastY,PieceType.ROOK, true);
+	    }
+	    //add bishop
+	    if(pieceT == 3 && player == 2){
+	    	piece.addPiece(lastX,lastY,PieceType.BISHOP, false);
+	    }
+	    if(pieceT == 3 && player == 1){
+	    	piece.addPiece(lastX,lastY,PieceType.PAWN, true);
+	    }
+	    //add queen
+	    if(pieceT == 4 && player == 2){
+	    	piece.addPiece(lastX,lastY,PieceType.QUEEN, false);
+	    }
+	    if(pieceT == 4 && player == 1){
+	    	piece.addPiece(lastX,lastY,PieceType.QUEEN, true);
+	    }*/
+    }
+    private Space convertXYToSpace(double x, double y)
+    {
+    	if(x < BOARD_SHIFT || x > BOARD_SHIFT+(100*8) || y < BOARD_SHIFT || y > BOARD_SHIFT+(100*8))
+    	{
+    		return new Space(-1, -1);
+    	}
+    	else
+    	{
+        	Space space = new Space((int)((y-BOARD_SHIFT)/100), (int)((x-BOARD_SHIFT)/100)); 
+        	return space;
+    	}
+    }
+    
+    private Piece getPieceFromXY(double x, double y)
+    {
+    	Space space = convertXYToSpace(x, y);
+    	return program.getBoard().getPiece(space);
+    }
+    
+    private int calculateRowsMoved()
+    {
+    	int rowsMoved = 0;
+    	Space start = convertXYToSpace(clickX, clickY); 
+    	Space end = convertXYToSpace(lastX, lastY); 
+    	Piece piece = getPieceFromXY(clickX, clickY);
+    	if(piece != null)
+    	{
+    		rowsMoved = end.getRow() - start.getRow();
+    	}
+    	else //If no piece was selected, the amount of rows moved is 0
+    	{
+    		rowsMoved = 0;
+    	}
+    	return rowsMoved;
+    }
+	    
+    //Code that returns how many cols a piece has moved
+    private int calculateColsMoved()
+    {
+    	int colsMoved = 0;
+    	Space start = convertXYToSpace(clickX, clickY);
+    	Space end = convertXYToSpace(lastX, lastY);
+    	Piece piece = getPieceFromXY(clickX, clickY);
+    	if(piece != null)
+    	{
+    		colsMoved = end.getCol() - start.getCol();
+    	}
+    	else //If no piece was selected, the amount of cols moved is 0
+    	{
+    		colsMoved = 0;
+    	}
+    	return colsMoved;
+    }
+    
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
